06f11bbea7fc0aa5c67a1d69e9c78097cd59dacb
Sven Dowideit <SvenDowideit@fosiki.com>
2012-01-17 12:06:29 +1000

    use archive - its fasterer

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync -vvr -e ssh svenud,foswiki@frs.sourceforge.net:/home/frs/project/f/fo/foswiki .
+rsync -vvar -e ssh svenud,foswiki@frs.sourceforge.net:/home/frs/project/f/fo/foswiki .
 git commit foswiki -m "update via rsync"
