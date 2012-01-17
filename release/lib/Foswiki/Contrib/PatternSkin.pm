bd532bd2bffb15fd215ac6669f63989ade4b012c
Sven Dowideit <SvenDowideit@fosiki.com>
2012-01-17 15:01:15 +1000

    foswiki/OldFiles/Foswiki-1.0.0.zip

---
+++ release/lib/Foswiki/Contrib/PatternSkin.pm
@@ -0,0 +1,4 @@
+package Foswiki::Contrib::PatternSkin;
+use vars qw( $VERSION );
+$VERSION = '$Rev: 1744 (08 Jan 2009) $';
+1;
---
+++ release/lib/Foswiki/Contrib/TipsContrib.pm
@@ -0,0 +1,4 @@
+package Foswiki::Contrib::TipsContrib;
+use vars qw( $VERSION );
+$VERSION = '$Rev: 1416 (08 Jan 2009) $';
+1;
